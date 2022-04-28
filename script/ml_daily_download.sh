ad0d71d966c364997b26a3a81163cba221a89683
Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
2022-04-28 09:32:03 +0900

    script/lib: enable multi url download
    This patch enables to download from multi url.
    
    Signed-off-by: Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>

--- script/ml_daily_download.sh
+++ script/ml_daily_download.sh
@@ -3,9 +3,10 @@
 #
 # ml_daily_download
 #
-#	> ml_daily_download alsa-devel@alsa-project.org ASoC 2022-01-09 2022-01-10
-#	> ml_daily_download alsa-devel@alsa-project.org ASoC 2022-01-09
-#	> ml_daily_download alsa-devel@alsa-project.org ASoC
+#	> ml_daily_download add-A dir 2022-01-09 2022-01-10
+#	> ml_daily_download add-A dir 2022-01-09
+#	> ml_daily_download add-A dir
+#	> ml_daily_download "add-A OR add-B" dir
 #	> ASoC/.alsa-devel@alsa-project.org.cmd
 #
 # 2022/02/03 Kuninori Morimoto <kuninori.morimoto.gx@renesas.com>
@@ -16,7 +17,7 @@ SCRIPT=`readlink -f "$0" | xargs dirname`
 CFG=.daily_download.lastday
 CMD=.daily_download.cmd
 
-URL=`${SCRIPT}/url_encode.py $1`
+URL=`${SCRIPT}/url_encode.py "$1"`
 DIR=`daily_outdir $2`
 DAY_FROM=`daily_dayfrom ${DIR}/${CFG} $3`
 DAY_TO=`daily_dayto $4`
